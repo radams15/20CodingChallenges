7d9b8e6686d69f8ef77a87ddaa938b47ba6082f1
Rhys Adams <email.curly@gmail.com>
2020-09-21 10:39:04 +0100

    Finished Email Validator!

--- Email Validator/Main.java
+++ Email Validator/Main.java
@@ -1,9 +1,63 @@
+import java.util.Scanner;
+import java.io.File;
+import java.util.ArrayList;
+
 class Main{
 
     public Main(){
-        Validator v = new Validator();
+        Validator validator = new Validator();
+        Scanner in = new Scanner(System.in);
         
-        System.out.println(v.getErrors("test1 @gmail.com"));
+        System.out.print("File? [Y/n]: ");
+        String choice = in.nextLine();
+        
+        if(choice.length() != 0 && choice.charAt(0) == 'y'){
+            System.out.print("File Name: ");
+            String fileName = in.nextLine();
+            if(fileName.length() != 0){
+                File file = new File(fileName);
+                if(! file.exists()){
+                    System.out.println("File Does Not Exist!");
+                    System.exit(1);
+                }
+                
+                try{
+                    Scanner fileIn = new Scanner(file);
+                    
+                    while(fileIn.hasNext()){
+                        String line = fileIn.nextLine();
+                        ArrayList<String> errors = validator.getErrors(line);
+                        System.out.printf("%s: ", line);
+                        if(errors.size() == 0){
+                            System.out.println("No Errors");
+                        }else{
+                        System.out.println("Errors:");
+                            for(String error : errors){
+                                System.out.println("\t"+error);
+                            }
+                        }
+                    }
+                }catch(java.io.FileNotFoundException e){
+                    e.printStackTrace();
+                    System.exit(1);
+                }
+            }
+        }else{
+            System.out.print("Email To Test: ");
+            String toTest = in.nextLine();
+            
+            if(toTest.length() != 0){
+                ArrayList<String> errors = validator.getErrors(toTest);
+                if(errors.size() == 0){
+                    System.out.println("No Errors");
+                }else{
+                System.out.println("Errors:");
+                    for(String error : errors){
+                        System.out.println("\t"+error);
+                    }
+                }
+            }
+        }
     }
     
     public static void main(String[] args){
--- Email Validator/Validator.java
+++ Email Validator/Validator.java
@@ -3,41 +3,46 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 class Validator{
-
-    private final Pattern emailRegex = Pattern.compile("[\\S]+@[\\S]+\\.[\\S]+", Pattern.MULTILINE);
     
-   
-    private String convertToHuman(String check){
-        if(check.equals("user")){
-            return "No Account Before The '@'";
-        }else if(check.equals("domain")){
-            return "No Domain Name Entered";
-        }else if(check.equals("extension")){
-            return "No Website Extension";
-        }
-        return "Unspecified Error";
-    }
+    private final Pattern emailRegex = Pattern.compile("[\\S]+@[\\S]+\\.[\\S]+", Pattern.MULTILINE);
+    private final Pattern dotRegex = Pattern.compile("[\\S]*@[\\S]*\\.", Pattern.MULTILINE);
+    private final Pattern extRegex = Pattern.compile("[\\S]*@[\\S]*\\.[\\S]+", Pattern.MULTILINE);
     
     public ArrayList<String> getErrors(String email){
         ArrayList<String> out = new ArrayList<>();
         
-        Matcher matcher = emailRegex.matcher(email);
+        Matcher regexMatcher = emailRegex.matcher(email);
         
         if(email.contains(" ")){
             out.add("Spaces In Email!");
+            return out;
         }
         
-        if(matcher.find()){
+        if(regexMatcher.find()){
             return out;
         }else{
-            if(email.contains("@")){
+            if(!email.contains("@")){
                 out.add("No '@' In Email!");
+            }else{
+                String[] split = email.split("@");
+                if(split[0].equals("")){
+                    out.add("Nothing Before The '@' Sign!");
+                }
+                if(split.length > 1 && split[1].equals("")){
+                    out.add("Nothing After The '@' Sign!");
+                }
             }
             
+            Matcher dotMatcher = dotRegex.matcher(email);
+            if(!dotMatcher.find()){
+                out.add("No Dot After '@' Sign!");
+            }
             
+            Matcher extMatcher = extRegex.matcher(email);
+            if(!extMatcher.find()){
+                out.add("No Domain Extension!");
+            }
         }
-        
-        
         
         return out;        
     }
