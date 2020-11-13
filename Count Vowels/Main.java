4030d8b8d845214de33bf5e6ae97f0d9446ca760
Rhys Adams <email.curly@gmail.com>
2020-11-13 10:35:30 +0000

    Done Count Vowels

--- Count Vowels/Main.java
+++ Count Vowels/Main.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-import java.util.ArrayList;
+import java.util.HashMap;
 
 import java.io.File;
 
@@ -16,32 +16,42 @@ class Main{
 			
 			Scanner fileIn = new Scanner(file);
 			
-			ArrayList<char> chars = new ArrayList<>();
+			HashMap<Character, Integer> chars = new HashMap<>();
 			
 			while(fileIn.hasNext()){
-				total += countVowels(fileIn.nextLine());
+				HashMap<Character, Integer> toMerge = (countVowels(fileIn.nextLine()));
+				toMerge.forEach((chr, count) -> {
+					if(chars.get(chr) == null){
+						chars.put(chr, count);
+					}else{
+						chars.put(chr, chars.get(chr)+count);
+					}
+				});
 			}
 			
 			
-			System.out.printf("In total the file contains %d Vowels\n", total);
+			System.out.println(chars);
 		}catch(java.io.IOException e){
 			System.out.println("File does not exist!");
 			System.exit(1);
 		}
 	}
 	
-	public int countVowels(String input){
-		int out = 0;
-		ArrayList<char> out = new ArrayList<>();
+	public HashMap<Character, Integer> countVowels(String input){
+		HashMap<Character, Integer> out = new HashMap<>();
 		
-		for(char s : input.toCharArray()){
+		for(char s : input.toLowerCase().toCharArray()){
 			switch(s){
 				case 'a':
 				case 'e':
 				case 'i':
 				case 'o':
 				case 'u':
-					out++;
+					if(out.get(s) == null){
+						out.put(s, 1);
+					}else{
+						out.put(s, out.get(s)+1);
+					}
 			}
 		}
 		return out;
