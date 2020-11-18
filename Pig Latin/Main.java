66a3d704f3b9e312d8237e7b322a3d244a3ddeef
Rhys Adams <email.curly@gmail.com>
2020-11-18 11:34:09 +0000

    Pig Latin Complete

--- Pig Latin/Main.java
+++ Pig Latin/Main.java
@@ -1,13 +1,18 @@
+import java.util.Scanner;
+
 class Main{
 	public Main(){
+		Scanner in = new Scanner(System.in);
+		
 		Translator translator = new Translator();
 		
-		
-		String input = "i am eating a banana on a stupid day";
-		
-		String output = translator.translate(input);
-		
-		System.out.println(output);
+		while(true){
+			System.out.print("=> ");
+			String input = in.nextLine();
+			String output = translator.translate(input);
+			
+			System.out.println(output);
+		}
 	}
 	
 	public static void main(String[] args){
