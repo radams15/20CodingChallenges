95fa6e115b72c02d97cd9994cc578b6f9f90d8f5
Rhys Adams <radams15@dgsb.co.uk>
2020-12-02 12:22:24 +0000

    Almost done change return

--- Change Return/Currency.java
+++ Change Return/Currency.java
@@ -1,8 +1,34 @@
+import java.util.ArrayList;
+
 class Currency{
 	
-	public float[] coins = new float[]{1.0, 0.5, 0.2, 0.1, 0.05, 0.01}; // the different coins available
+	public double[] coins; // the different coins available
 
-	public Currency(){
+	public Currency(double[] coinSet){
+		coins = coinSet;
+	}
+	
+	private double largestCoinFits(double number){
+		for(double coin : coins){
+			if(number-coin > -0.009){ // account for double precision loss, fairly badly
+				return coin;
+			}
+		}
 		
+		return -1;
+	}
+
+	public ArrayList<Double> getCoins(double change){
+		ArrayList<Double> out = new ArrayList<>();
+		
+
+		while(true){
+			double largestCoin = largestCoinFits(change);
+			if(largestCoin == -1) break;
+			out.add(largestCoin);
+			change -= largestCoin;
+		}
+
+		return out;
 	}
 }
--- Change Return/Main.java
+++ Change Return/Main.java
@@ -1,6 +1,58 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Random;
+
 class Main{
 	public Main(){
-		
+		/*Scanner in = new Scanner(System.in);
+		Currency ukCurrency = new Currency(new double[]{2.0, 1.0, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01});
+		Currency usCurrency = new Currency(new double[]{1.0, 0.5, 0.25, 0.1, 0.05, 0.01});
+
+		System.out.print("Cost: ");
+		double cost = in.nextDouble();
+		in.nextLine();
+
+		System.out.print("Given: ");
+		double given = in.nextDouble();
+		in.nextLine();
+
+		double change = given-cost;
+
+		System.out.printf("Change Is: %.2f\n", change);
+
+		System.out.println("US: "+usCurrency.getCoins(change));
+		System.out.println("UK: "+ukCurrency.getCoins(change));*/
+
+		System.out.println(test());
+	}
+
+	public boolean test(){
+		Random rand = new Random();
+
+		Currency ukCurrency = new Currency(new double[]{2.0, 1.0, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01});
+		Currency usCurrency = new Currency(new double[]{1.0, 0.5, 0.25, 0.1, 0.05, 0.01});
+
+		double given = rand.nextInt(100)+rand.nextFloat();
+		double price = rand.nextInt((int)given)+rand.nextFloat();
+
+		double change = given-price;
+
+		double usSum = usCurrency.getCoins(change).stream()
+		.mapToDouble(a -> a)
+		.sum();
+
+		double ukSum = ukCurrency.getCoins(change).stream()
+		.mapToDouble(a -> a)
+		.sum();
+
+		System.out.printf("Change: %f, Actual: %f / %f\n", change, ukSum, usSum);
+
+		if(usSum != change || ukSum != change){
+			return false;
+		}
+
+
+		return true;
 	}
 
 	public static void main(String[] args){
