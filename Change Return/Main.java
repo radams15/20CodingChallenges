7db53addec57a1e9368709d2bd640540e9bbe817
Rhys Adams <radams15@dgsb.co.uk>
2020-12-04 12:34:35 +0000

    Completed Change Return Program

--- Change Return/Main.java
+++ Change Return/Main.java
@@ -1,10 +1,14 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Random;
+import java.lang.Math;
 
 class Main{
 	public Main(){
-		/*Scanner in = new Scanner(System.in);
+		if(!test()){
+			System.out.println("Test Failed!");
+		}
+		Scanner in = new Scanner(System.in);
 		Currency ukCurrency = new Currency(new double[]{2.0, 1.0, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01});
 		Currency usCurrency = new Currency(new double[]{1.0, 0.5, 0.25, 0.1, 0.05, 0.01});
 
@@ -21,9 +25,7 @@ class Main{
 		System.out.printf("Change Is: %.2f\n", change);
 
 		System.out.println("US: "+usCurrency.getCoins(change));
-		System.out.println("UK: "+ukCurrency.getCoins(change));*/
-
-		System.out.println(test());
+		System.out.println("UK: "+ukCurrency.getCoins(change));
 	}
 
 	public boolean test(){
@@ -32,8 +34,8 @@ class Main{
 		Currency ukCurrency = new Currency(new double[]{2.0, 1.0, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01});
 		Currency usCurrency = new Currency(new double[]{1.0, 0.5, 0.25, 0.1, 0.05, 0.01});
 
-		double given = rand.nextInt(100)+rand.nextFloat();
-		double price = rand.nextInt((int)given)+rand.nextFloat();
+		double given = round(rand.nextInt(100)+rand.nextFloat(), 2);
+		double price = round(rand.nextInt((int)given)+rand.nextFloat(), 2);
 
 		double change = given-price;
 
@@ -45,14 +47,18 @@ class Main{
 		.mapToDouble(a -> a)
 		.sum();
 
-		System.out.printf("Change: %f, Actual: %f / %f\n", change, ukSum, usSum);
+		//System.out.printf("Change: %f, Actual: %f / %f\n", change, ukSum, usSum);
 
 		if(usSum != change || ukSum != change){
 			return false;
 		}
 
-
 		return true;
+	}
+
+	double round(double value, int places) {
+	    double scale = Math.pow(10, places);
+	    return Math.round(value * scale) / scale;
 	}
 
 	public static void main(String[] args){
